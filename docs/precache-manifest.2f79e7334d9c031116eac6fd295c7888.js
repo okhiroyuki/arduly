1ad4251d5409c682ae07a43e9621f959545d968e
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2021-01-03 14:34:57 +0900

    feat: Support PWA

---
+++ docs/precache-manifest.2f79e7334d9c031116eac6fd295c7888.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db7a7165303e4521e83",
+    "url": "css/app.a6c4d8fb.css"
+  },
+  {
+    "revision": "971bab6c2e61c73722d0",
+    "url": "css/chunk-vendors.42677c9e.css"
+  },
+  {
+    "revision": "019d63c2b17ede8d75eb926d5cd89561",
+    "url": "index.html"
+  },
+  {
+    "revision": "7db7a7165303e4521e83",
+    "url": "js/app.22328e71.js"
+  },
+  {
+    "revision": "971bab6c2e61c73722d0",
+    "url": "js/chunk-vendors.115cfc2b.js"
+  },
+  {
+    "revision": "966a37761110234a0bb79e56195e31ea",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
