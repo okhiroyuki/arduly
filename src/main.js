d3b8140a83c5093d6792b5c4f9c612ee1b95f691
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2021-01-03 17:10:22 +0900

    feat: add vue-gtag

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import VueGtag from 'vue-gtag';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import './registerServiceWorker';
@@ -6,6 +7,10 @@ import './registerServiceWorker';
 Vue.config.productionTip = false;
 // Add unimported components to ignore list to prevent warnings.
 Vue.config.ignoredElements = ['field', 'block', 'category', 'xml', 'mutation', 'value', 'sep'];
+
+Vue.use(VueGtag, {
+  config: { id: 'G-0X3L4Z9H78' },
+});
 
 new Vue({
   vuetify,
